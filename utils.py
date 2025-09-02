f23aa81afb99bbaa40d1c6e8b6aecf79e2106399
brenaldo19 <breno.alkmin@gmail.com>
2025-09-01 21:52:20 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from fractions import Fraction
+import streamlit as st   # mantém aqui
 
-def entrada_numero(st, rotulo, valor_inicial="0", chave=None):
+def entrada_numero(rotulo, valor_inicial="0", chave=None):
     """
     Entrada de número que aceita frações (ex: 3/4) ou decimais (ex: 2.5).
     Retorna float ou None se inválido.
