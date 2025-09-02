76bebffe518db29db827076606b9c702164f5540
brenaldo19 <breno.alkmin@gmail.com>
2025-09-01 21:45:28 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 from fractions import Fraction
 
-def entrada_numero(rotulo, valor_inicial="0", chave=None):
+def entrada_numero(st, rotulo, valor_inicial="0", chave=None):
+    """
+    Entrada de número que aceita frações (ex: 3/4) ou decimais (ex: 2.5).
+    Retorna float ou None se inválido.
+    """
     valor_str = st.text_input(rotulo, valor_inicial, key=chave)
     try:
         return float(Fraction(valor_str))
